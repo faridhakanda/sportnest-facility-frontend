fa0c6aeb32f84435cc90a615d560ebe830a7ddb8
faridhakanda <faridakanda1729@gmail.com>
2026-05-18 17:27:48 +0600

    route.js file updated

--- src/app/api/auth/[...all]/route.js
+++ src/app/api/auth/[...all]/route.js
@@ -1,5 +1,5 @@
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
-
+// here is show problen in vercel deployment
 export const { POST, GET } = toNextJsHandler(auth);
