e4638b30ece530d6c6b04acf8ab81fbb8c178e9c
faridhakanda <faridakanda1729@gmail.com>
2026-05-18 18:18:50 +0600

    updated footer file

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <div className='bg-blue-800 text-white p-2 text-center w-full'>
             <h2>Footer page for sportnest facility project!</h2>
+            <p>&copy; All Right Reserved. 2026 with SportNest Ltd.</p>
         </div>
     );
 };
